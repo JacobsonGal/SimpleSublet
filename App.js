2a07c4f5c17000555175c2b9ae849983c600d87f
JacobsonGal <jacobsongal@gmail.com>
2021-01-03 22:55:13 +0200

    INIT REACT NATIVE

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Alert,
+  Button,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Platform,
+  StatusBar as Status,
+  Dimensions,
+} from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+import WelcomeScreen from "./App/views/WelcomeScreen";
+import ViewImageScreen from "./App/views/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+{
+  /*
+   return (
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: orientation ? "100%" : "30%",
+        }}
+      ></View>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+    const containerStyle = { backgroundColor: "dodgerblue" };
+
+   <Text numberOfLines={1}>Welcome to Simple Sublet!</Text>
+   <TouchableOpacity>
+        <Button
+          color="blue"
+          title={"Click me"}
+          // onPress={() =>
+          //   Alert.alert("My Title", "My Massage", [
+          //     { text: "Yes", onPress: () => console.log("Yes") },
+          //     { text: "No", onPress: () => console.log("No") },
+          //   ])
+          // }
+          onPress={() =>
+            Alert.prompt("My Title", "My Massage", (text) => console.log(text))
+          }
+        ></Button>
+        <View
+          style={{ width: 200, height: 70, backgroundColor: "black" }}
+        ></View>
+        <Image
+          blurRadius={1}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity> */
+}
---
+++ App/views/WelcomeScreen.js
@@ -0,0 +1,46 @@
+// rsf
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+
+export default function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text> Sell what you dont need </Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+// rnss
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+});
